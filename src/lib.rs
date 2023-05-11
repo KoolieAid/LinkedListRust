2a43e53b5fcdeb736df83f2520424023af893948
Go Erik <erkgo.eg@gmail.com>
2023-05-11 14:28:30 +0800

    finalized ll

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,104 @@
-use std::cell::RefCell;
+#![allow(unused)]
+
+use std::cell::{RefCell, RefMut};
 use std::rc::{Rc, Weak};
+
+use std::fmt::Debug;
 
 #[cfg(test)]
 mod tests;
 
 #[derive(Debug)]
 struct Node<T> {
-    data: Rc<T>,
-    child: Option<RefCell<Box<Node<T>>>>,
-}
-
-impl<T> Node<T> {
-    fn generate_single(data: T) -> Node<T> {
-        Node {
-            data: Rc::new(data),
-            child: None,
-        }
-    }
+    data: Rc<RefCell<T>>,
+    next: Option<Box<Node<T>>>,
 }
 
 #[derive(Debug)]
 pub struct LinkedList<T> {
     head: Node<T>,
-    count: i32,
+    size: usize,
 }
 
 impl<T> LinkedList<T> {
-    pub fn new(first: T) -> LinkedList<T> {
-        let node = Node::generate_single(first);
+    pub fn new(data: T) -> LinkedList<T> {
+        let head = Node {
+            data: Rc::new(RefCell::new(data)),
+            next: None,
+        };
 
-        LinkedList {
-            head: node,
-            count: 1,
-        }
+        LinkedList { head, size: 1 }
     }
 
-    pub fn add(&mut self, element: T) {
-        todo!("Please help me");
-        let new_node = Node::generate_single(element);
+    pub fn add(&mut self, data: T) -> Result<(), &'static str>
+    where
+        T: Debug,
+    {
+        let new = Node {
+            data: Rc::new(RefCell::new(data)),
+            next: None,
+        };
 
-        if let None = self.head.child {
-            return;
+        let mut iter: &mut Node<T> = &mut self.head;
+
+        while iter.next.is_some() {
+            // println!("yo");
+            iter = iter.next.as_mut().unwrap();
         }
 
-        let mut current: Node<T>;
+        iter.next = Some(Box::new(new));
+        self.size += 1;
 
-//        while *(self.head.child.borrow_mut().) != None {
-//            println!("please help");
-//        }
+        Ok(())
     }
 
-}
+    pub fn remove(&mut self, index: i32) -> Result<T, &'static str>
+    where
+        T: Debug,
+    {
+        let size = self.size as i32;
 
-impl<T> std::ops::Index<i32> for LinkedList<T> {
-    type Output = T;
-    fn index(&self, index: i32) -> &Self::Output {
-        &self.head.data
+        match index {
+            i if i < 0 => return Err("Less than 0"),
+            i if i >= self.size as i32 => return Err("More than size"),
+            _ => {}
+        }
+
+        let mut iter: &mut Node<T> = &mut self.head;
+
+        for i in 0..index - 1 {
+            iter = iter.next.as_mut().unwrap();
+        }
+
+        let item: Box<Node<T>> = iter
+            .next
+            .take()
+            .ok_or("Yea I havent figured out empty lists and im lazy to do it")?;
+
+        let data = Rc::try_unwrap(item.data).expect("item.data has multiple references");
+
+        self.size -= 1;
+        Ok(data.into_inner())
+    }
+
+    pub fn get(&mut self, index: i32) -> Result<RefMut<T>, &'static str>
+    where
+        T: Debug,
+    {
+        let size = self.size as i32;
+
+        match index {
+            i if i < 0 => return Err("Less than 0"),
+            i if i >= self.size as i32 => return Err("More than size"),
+            _ => {}
+        }
+
+        let mut iter: &mut Node<T> = &mut self.head;
+
+        for i in 0..index {
+            iter = iter.next.as_mut().unwrap();
+        }
+
+        Ok(iter.data.borrow_mut())
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,15 +1,59 @@
-use super::*;
+use super::LinkedList;
 
-#[test]
-fn construct() {
-    let list = LinkedList::new(3);
-
-    dbg!(&list);
-    assert!(true);
+fn create_list<T: Default>() -> LinkedList<T> {
+    LinkedList::new(T::default())
 }
 
 #[test]
-fn get_first_element() {
-    let list = LinkedList::new(42);
-    assert_eq!(list[0], 42);
+fn get_item() {
+    let mut list = create_list();
+
+    list.add(45);
+
+    assert_eq!(i32::default(), *list.get(0).unwrap());
+    assert_eq!(45, *list.get(1).unwrap());
+}
+
+#[test]
+#[should_panic]
+fn get_non_existing() {
+    let mut list = create_list::<i32>();
+
+    let data = *list.get(1).unwrap();
+}
+
+#[test]
+#[should_panic]
+fn negative_index() {
+    let mut list = create_list::<i32>();
+
+    let data = *list.get(-1).unwrap();
+}
+
+#[test]
+fn add_element() {
+    let mut list = create_list();
+
+    assert_eq!(
+        i32::default(),
+        *list.get(0).unwrap(),
+        "First element is not default value"
+    );
+
+    list.add(55);
+
+    assert_eq!(55, *list.get(1).unwrap());
+}
+
+#[test]
+fn remove_element() {
+    let mut list = create_list();
+
+    list.add(100);
+
+    assert_eq!(100, *list.get(1).unwrap());
+
+    list.remove(1);
+
+    assert!(list.get(1).is_err());
 }
